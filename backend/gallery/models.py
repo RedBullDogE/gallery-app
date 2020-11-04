09624f678bb92c6ba1ceac9e0cf4f370c0616697
An Drew <gsb06092000@gmail.com>
2020-11-04 23:09:04 +0200

    backend: add PIllow package and implement Picture model

--- backend/gallery/models.py
+++ backend/gallery/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Picture(models.Model):
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='pictures'
+    )
+    description = models.TextField(
+        max_length=1000,
+        null=True,
+        blank=True
+    )
+    date = models.DateField(
+        auto_now_add=True
+    )
+    file = models.ImageField(
+        upload_to='pictures'
+    )
