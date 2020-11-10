2d10cd8a2604c9559664c70bd045d9c981f7cba1
An Drew <gsb06092000@gmail.com>
2020-11-10 16:19:24 +0200

    frontend: basic setup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,28 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <h1>Gallery App</h1>
+        <router-view />
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+html {
+    font-size: 62.5%;
+    font-family: 'Open Sans', sans-serif;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+h1 {
+    font-size: 4rem;
+    font-weight: 800;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+h2 {
+    font-size: 2.6rem;
 }
 </style>
